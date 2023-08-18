de42958ea197ca165a5af3f2404bb298377ea3a4
zabojnikp <petra@productboard.com>
2023-08-18 15:49:51 +0200

    remove login page object

--- tests/specs/app.login.spec.ts
+++ tests/specs/app.login.spec.ts
@@ -1,7 +1,6 @@
 import * as TrackingPage from '../pageobjects/tracking.page';
 import * as IntroPage from '../pageobjects/intro.page';
 describe('Login', () => {
-
     it('should be able use app without login', async () => {
         await TrackingPage.assertPageLoaded();
         await TrackingPage.clickAcceptAllButton();
