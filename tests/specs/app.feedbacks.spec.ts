56e9e91098221a55ec8372fa0a58f092e6c0a4c1
cd <cd@cds-MacBook-Pro.local>
2024-02-16 15:55:06 +0100

    mentoring notes# git add . exit quit
    Öx
    :x

--- tests/specs/app.feedbacks.spec.ts
+++ tests/specs/app.feedbacks.spec.ts
@@ -5,6 +5,8 @@ import * as LoginPage from '../pageobjects/login.page';
 import * as PhoneNumberPage from '../pageobjects/phone.number.input.page';
 import * as SmsCodePage from '../pageobjects/sms.code.page';
 
+//locators
+
 describe('Feedbacks', () => {
     before(async () => {
 
@@ -26,7 +28,7 @@ describe('Feedbacks', () => {
         await expect($(MenuPage)).toExist;
     });
 
-    it('feedbackMenuItem open and send', async () => {
+    it('should send a feedback', async () => {
         //feedbackMenuItem 
         const feedbackMenuItem = '//android.view.ViewGroup[@resource-id="feedbackMenuItem"]';
         const FeedbackTextName = '/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup[1]/android.widget.FrameLayout/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup[2]/android.view.ViewGroup[1]/android.widget.ScrollView/android.view.ViewGroup/android.widget.TextView[1]';
@@ -36,17 +38,18 @@ describe('Feedbacks', () => {
         const FeedbackConfirmPage = '/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup[1]/android.widget.FrameLayout/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup[1]/android.widget.ScrollView/android.view.ViewGroup/android.widget.TextView[1]';
         const SkipStepButton = '/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup[1]/android.widget.FrameLayout/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup[2]/android.widget.TextView';
         
-        await $('android=new UiScrollable(new UiSelector().scrollable(true)).scrollTextIntoView("Právní informace")');
-        await browser.pause(1000);
+        //await $('android=new UiScrollable(new UiSelector().scrollable(true)).scrollTextIntoView("Právní informace")');
+        await browser.pause(1000); //dont use pause, use waitFor functions
         await $(feedbackMenuItem).click(); 
-        await expect($(FeedbackTextName)).toHaveTextContaining('Váš názor nás zajímá');
+        await expect($(FeedbackTextName)).toHaveTextContaining('Váš názor nás zajímá');//check for visibility
         await $(FiveStarsSend).click();
         await $(FeedbackForm).click();
         //FeedbackForm - Promocodefield
         await $(FeedbackForm).setValue('Albert je nejlepsi!');
         await $(SendButton).click();
-        await expect($(FeedbackConfirmPage)).toHaveTextContaining('DĚKUJEME');
+        await expect($(FeedbackConfirmPage)).toHaveTextContaining('DĚKUJEME');//check for visibility
         await expect($(SkipStepButton)).toHaveTextContaining('Přeskočit tento krok');
         await $(SkipStepButton).click();
+        // check user is redirected , page not visible, menu page visible
     });
 });
